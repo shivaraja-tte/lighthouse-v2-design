e1f97f4fc8be5926055b5ca2b5139083fe591926
shivaraja-tte <shivaraja.nataraja@mercer.com>
2026-03-11 17:11:10 +0530

    Update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lighthouse-preview/',
+  base: '/lighthouse-v2-design/',
 })
